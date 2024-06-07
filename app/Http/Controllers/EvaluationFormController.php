729991ae4e8c599e7ebdf8bdfafb54998ee42dc9
wachiravivian <sheilawamuyu4@gmail.com>
2024-06-07 15:32:09 +0300

    functionalities

--- app/Http/Controllers/EvaluationFormController.php
+++ app/Http/Controllers/EvaluationFormController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EvaluationForm;
 
-class EvaluationController extends Controller
+class EvaluationFormController extends Controller
 {
     public function submit(Request $request)
     {
@@ -37,9 +38,30 @@ class EvaluationController extends Controller
             $severity = 'Severe Depression';
         }
 
+        // Store the form submission in the database
+        EvaluationForm::create([
+            'q1' => $validatedData['q1'],
+            'q2' => $validatedData['q2'],
+            'q3' => $validatedData['q3'],
+            'q4' => $validatedData['q4'],
+            'q5' => $validatedData['q5'],
+            'q6' => $validatedData['q6'],
+            'q7' => $validatedData['q7'],
+            'q8' => $validatedData['q8'],
+            'q9' => $validatedData['q9'],
+            'total_score' => $totalScore,
+            'severity' => $severity,
+        ]);
+
         // Return the results with a success message
         return back()->with('success', 'Form submitted successfully!')
                      ->with('totalScore', $totalScore)
                      ->with('severity', $severity);
+    }
+
+    public function showReport()
+    {
+        $evaluationForms = EvaluationForm::all();
+        return view('evaluation-report', compact('evaluationForms'));
     }
 }
