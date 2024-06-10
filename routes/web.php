f496954d975910182b3719537167b0a1e7d7f621
wachiravivian <sheilawamuyu4@gmail.com>
2024-06-10 20:57:28 +0300

    admin

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,7 @@ Route::post('/journals', [JournalController::class, 'store'])->name('journals.st
  //Route::get('/admin/view-user-records', [ViewUserRecordsController::class, 'show'])->name('admin.viewUserRecords'); //
  Route::get('/admin/users', [ViewUserRecordsController::class, 'index'])->name('admin.users.index');
  Route::get('/admin/users/{user}', [ViewUserRecordsController::class, 'show'])->name('admin.users.show');
+ Route::get('/admin/manage-user-permissions', [ManageUserPermissionsController::class, 'index'])->name('admin.manageUserPermissions');
  Route::get('/admin/users/{user}/edit', [ManageUserPermissionsController::class, 'edit'])->name('admin.users.edit');
  Route::put('/admin/users/{user}', [ManageUserPermissionsController::class, 'update'])->name('admin.users.update');
 
